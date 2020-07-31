c09273ba5c1e650a2f01af61c6f4b1de61146b85
skcc08144 <skcc08144@SKVDIWEB.COM>
2020-07-31 14:30:00 +0900

    2nd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Min&seo&wook!');
+    res.send('I like Min&seo&wook @@@@!');
 });
 
 app.get('/start', function(req, res){
