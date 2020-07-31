c6a278ff5ef76c33dac6e9d85af4865b5ca0a4cb
BrianK89 <68985362+BrianK89@users.noreply.github.com>
2020-07-31 14:49:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Min&seo&wook EST02!');
+    res.send('I like Min EST02!');
 });
 
 app.get('/start', function(req, res){
